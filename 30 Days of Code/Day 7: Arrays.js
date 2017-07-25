c9e5e557d652ce9029f007703975ab143e1adbda
Firat Yalavuz <yalavuz@gmail.com>
2017-07-25 15:10:21 +0300

    30 Days of Code: Day 7

---
+++ 30 Days of Code/Day 7: Arrays.js
@@ -0,0 +1,34 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var n = parseInt(readLine());
+    arr = readLine().split(' ');
+    arr = arr.map(Number);
+    
+    var output = [];
+    for (var i = n; i > 0; i--) {
+        output.push(arr[i - 1]);
+    }
+    
+    console.log(output.join(' '));
+}
