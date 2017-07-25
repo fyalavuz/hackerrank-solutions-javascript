db4e6b6a52d1958250d8bf5e68b2afe7c8d18804
Firat Yalavuz <yalavuz@gmail.com>
2017-07-25 13:46:14 +0300

    30 Days of Challange

---
+++ 30 Days of Code/Day 3: Intro to Conditional Statements.js
@@ -0,0 +1,30 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function main() {
+    var N = parseInt(readLine());
+    if (N % 2 != 0 || N % 2 == 0 && N >= 5 && N <= 20) {
+        console.log('Weird');
+    } else {
+        console.log('Not Weird');
+    }
+}
